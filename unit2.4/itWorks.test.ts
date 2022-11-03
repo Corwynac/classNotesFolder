0eee7596c92be95f538ff33dc29620a549251b65
Corwyn <corwynac@gmail.com>
2022-11-03 14:49:40 -0500

    notes

--- unit2.4/itWorks.test.ts
+++ unit2.4/itWorks.test.ts
@@ -4,6 +4,6 @@ const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
 test("can it open employee manager", async () => {
     await driver.get("https://devmountain-qa.github.io/employee-manager/1.2_Version/index.html")
-    await driver.sleep(10000)
+    await driver.sleep(1000)
     await driver.quit()
 })
